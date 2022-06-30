8c40128239fde66bdcf9f569f554752c3329dfdc
Miguel Angel <58991934+0M1N1D0@users.noreply.github.com>
2022-06-30 18:24:50 -0500

    urls y vistas iniciales

--- src/vista/views.py
+++ src/vista/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+""" Aquí estarán las vistas estáticas """
+
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def home(request): # pylint: disable=unused-argument
+    """ vista home """
+    return HttpResponse("Inicio")
+
+def about(request): # pylint: disable=unused-argument
+    """ vista about """
+    return HttpResponse("Historia")
+
+def services(request): # pylint: disable=unused-argument
+    """ vista services """
+    return HttpResponse("Servicios")
+
+def store(request): # pylint: disable=unused-argument
+    """ vista store """
+    return HttpResponse("Visítanos")
+
+def contact(request): # pylint: disable=unused-argument
+    """ vista contact """
+    return HttpResponse("Contacto")
+
+def blog(request): # pylint: disable=unused-argument
+    """ vista blog """
+    return HttpResponse("Blog")
+
+def sample(request): # pylint: disable=unused-argument
+    """ vista sample """
+    return HttpResponse("Sample")
